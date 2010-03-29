490af5b2e780e18bf6a7e72a9c854ce3d5357717
Jeroen Jacobs <jacobsjeroen@gmail.com>
2010-03-29 16:37:15 +0200

    Scopes for limit and kind

--- app/models/description.rb
+++ app/models/description.rb
@@ -1,6 +1,7 @@
 class Description < ActiveRecord::Base
-  set_table_name "products_description"
   LANGUAGES = {'fr' => 1, 'nl' => 2, 'en' => 3}
+
+  set_table_name "products_description"
 
   belongs_to :product
 
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,15 @@
 class Product < ActiveRecord::Base
+  KINDS = {'dvd' => 'DVD_NORM', 'adult' => 'DVD_ADULT', 'subscription' => 'ABO'}
+
   set_primary_key "products_id"
 
   alias_attribute :kind,  :products_type
   alias_attribute :title, :products_title
 
   has_many :descriptions, :foreign_key => :products_id
+
+  named_scope :limit, lambda {|limit| {:limit => limit}}
+  named_scope :by_kind, lambda {|kind| {:conditions => {:products_type => KINDS[kind]}}}
 
   def description
     descriptions.by_language(I18n.locale.to_s)
