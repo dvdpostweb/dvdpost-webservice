c4062390084a4204916ebadd870de28ccfe18883
Jeroen Jacobs <jacobsjeroen@gmail.com>
2010-03-30 12:40:46 +0200

    Added soundtrack to product

---
+++ app/models/soundtrack.rb
@@ -0,0 +1,9 @@
+class Soundtrack < ActiveRecord::Base
+  set_table_name :products_soundtracks
+
+  set_primary_key :soundtracks_id
+
+  alias_attribute :name, :soundtracks_description
+
+  has_and_belongs_to_many :products, :join_table => :products_to_soundtracks, :foreign_key => :products_soundtracks_id, :association_foreign_key => :products_id
+end
