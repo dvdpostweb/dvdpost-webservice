5e1470fa4b262d8cf2f939cd1aac7be5c365753c
Jeroen Jacobs <jacobsjeroen@gmail.com>
2010-04-01 14:37:04 +0200

    Replaced Prototype with JQuery and included jrails to enable the rails helpers on jQuery. Moved all js to application.js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,24 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$(document).ready(function(){
+  // hides the slickbox as soon as the DOM is ready
+  // (a little sooner than page load)
+  $('#lang-box').hide();
+
+  // toggles the slickbox on clicking the noted link  
+  $('a#lang').click(function() {
+    $('#lang-box').toggle(50);
+    return false;
+  });
+
+  $("#indicator #n6").click(function(event){
+    $("#indicator-tips").toggle(0);
+    return false;
+  });
+
+  $("#close").click(function(event){
+    $("#indicator-tips").hide();
+    return false;
+  });
+});
