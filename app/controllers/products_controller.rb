297f96b498e1c1a8ad330a2533ad4a385d769b33
Jeroen Jacobs <jacobsjeroen@gmail.com>
2010-04-06 15:40:36 +0200

    Ability to search on multiple kinds

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,7 @@ class ProductsController < ApplicationController
   def index
     @products = Product.by_kind(:normal).limit(10)
     @products = @products.search(params[:search]) if params[:search]
-    @products = @products.by_media(params[:media]) if params[:media]
+    @products = @products.by_media(params[:media].split(',')) if params[:media]
   end
 
   def show
