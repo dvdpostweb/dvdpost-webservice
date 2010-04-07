25bfd895f13fcee9eced9461a4ad6ab37ae7d70f
Jeroen Jacobs <jacobsjeroen@gmail.com>
2010-04-07 10:15:46 +0200

    Removed useless has_many from user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 class User < ActiveRecord::Base
   include Clearance::User
 
-  has_many :orders, :foreign_key => :customers_id
-
   def authenticated?(password)
     customer = Customer.find_by_email(email)
     customer ? customer.authenticated?(password) : false
