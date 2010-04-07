280c355b320262693fb8bee258decfd90c26664b
Jeroen Jacobs <jacobsjeroen@gmail.com>
2010-04-07 16:50:30 +0200

    Implemented reviews with basic pagination.

---
+++ app/models/review.rb
@@ -0,0 +1,27 @@
+class Review < ActiveRecord::Base
+  cattr_reader :per_page
+  @@per_page = 3
+
+  establish_connection :dvdpost_main
+
+  set_primary_key :reviews_id
+
+  alias_attribute :created_at, :date_added
+  alias_attribute :text, :reviews_text
+  alias_attribute :rating, :reviews_rating
+  alias_attribute :like_count, :customers_best_rating
+  alias_attribute :dislike_count, :customers_bad_rating
+
+  belongs_to :customer, :foreign_key => :customers_id
+  belongs_to :product, :foreign_key => :product_id
+
+  default_scope :order => 'customers_best_rating ASC, customers_bad_rating DESC'
+
+  def likeable_count
+    like_count + dislike_count
+  end
+
+  def likeability
+    like_count - dislike_count
+  end
+end
