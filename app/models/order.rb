9ce86bc3af1d5cc94f71a169aaadfac7af176921
Jeroen Jacobs <jacobsjeroen@gmail.com>
2010-04-07 11:27:16 +0200

    Order#changed_at because not all orders have a updated_at field. It will fall back on the created_at date

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,10 +4,15 @@ class Order < ActiveRecord::Base
   set_primary_key :orders_id
 
   alias_attribute :updated_at, :last_modified
+  alias_attribute :updated_at, :date_purchased
 
   belongs_to :customer, :foreign_key => :customers_id
   has_one :order_product, :foreign_key => :orders_id
   has_one :product, :through => :order_product, :source => :product
 
   named_scope :in_transit, :conditions => {:orders_status => [1,2]}
+
+  def changed_at
+    updated_at || created_at
+  end
 end
