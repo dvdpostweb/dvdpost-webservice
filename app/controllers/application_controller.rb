3fd5f67a924ed0b3f9fa647bd26446fe8f7c1625
Jeroen Jacobs <jacobsjeroen@gmail.com>
2010-04-08 12:10:46 +0200

    current_customer is now available in the views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   include Clearance::Authentication
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
+  helper_method :current_customer
 
   before_filter :authenticate
   before_filter :wishlist_size
