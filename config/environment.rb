2fc9e30258c4620420ef2ca696b25b45185e8c96
Jeroen Jacobs <jacobsjeroen@gmail.com>
2010-04-13 15:04:55 +0200

    Defined dependencies for the gems. Vendored hoptoad notifier

--- config/environment.rb
+++ config/environment.rb
@@ -15,10 +15,10 @@ Rails::Initializer.run do |config|
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
 
   # Specify gems that this application depends on and have them installed with rake gems:install
-  config.gem 'capistrano-ext', :lib => 'capistrano'
-  config.gem 'clearance',                           :version => '>= 0.8.8'
-  config.gem 'will_paginate'
-  config.gem 'hoptoad_notifier'
+  config.gem 'capistrano-ext',    :lib => 'capistrano', :version => '>= 1.2.1'
+  config.gem 'hoptoad_notifier',                        :version => '>= 2.2.2'
+  config.gem 'clearance',                               :version => '>= 0.8.8'
+  config.gem 'will_paginate',                           :version => '>= 2.3.12'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
