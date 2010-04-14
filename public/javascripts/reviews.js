356e891b231ba53353ea57061298a8db7cc18d83
Jeroen Jacobs <jacobsjeroen@gmail.com>
2010-04-14 15:45:12 +0200

    Code cleanup in review ratings

--- public/javascripts/reviews.js
+++ public/javascripts/reviews.js
@@ -7,4 +7,24 @@ $(function() {
     });
     return false;
   });
+
+  $(".yn .yes, .yn .no").live("click",function(){
+    $(this).parent("p.yn").html('<img src="/images/ajax-loader.gif" />');
+    review_id = $(this).attr('review_id');
+    html_item = $('#critique'+review_id);
+    content = html_item.html();
+    $.ajax({
+      url: this.href,
+      contentType: 'application/json; charset=utf-8',
+      type: 'POST',
+      data: JSON.stringify({"review_rating": {"rate": $(this).attr('rate')}}),
+      success: function(data) {
+        html_item.replaceWith(data);
+      },
+      error: function() {
+        html_item.html(content);
+      }
+    });
+    return false;
+  });
 });
