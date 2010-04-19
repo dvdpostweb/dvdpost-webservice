7bc7be77980948654d5be49cde45820c7beaa82e
Jeroen Jacobs <jacobsjeroen@gmail.com>
2010-04-19 11:21:40 +0200

    Added static homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
+  def index
+    @body_id = 'one-col'
+  end
+
   def indicator_closed
     session[:indicator_stored] = true
     render :nothing => true
