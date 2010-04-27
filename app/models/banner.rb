1d206f0fe16d3ef9505198ddf4ceebe4f4271ea3
Jeroen Jacobs <jacobsjeroen@gmail.com>
2010-04-27 13:43:21 +0200

    Renamed 'Banner' to 'banner'

---
+++ app/models/banner.rb
@@ -0,0 +1,17 @@
+class Banner < ActiveRecord::Base
+  set_primary_key :banners_id
+
+  alias_attribute :url, :banners_url
+  alias_attribute :group, :banners_group
+  alias_attribute :title, :banners_title
+  alias_attribute :size, :banners_group
+  alias_attribute :banner, :banners_image
+
+  named_scope :by_language, lambda {|language| {:conditions => {(language == :nl ? :active_nl : (language == :en ? :active_en : :status)) => 1}}}
+  named_scope :by_size, lambda {|size| {:conditions => {:banners_group => DVDPost.banner_size[size]}}}
+  named_scope :expiration, :conditions => ["expires_date > ? or expires_date is null ", Time.now]
+
+  def image
+    File.join(DVDPost.images_shop_path[I18n.locale], banner) unless banner.empty?
+  end
+end
