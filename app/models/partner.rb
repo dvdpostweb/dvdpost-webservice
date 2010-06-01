342c49211c2beb300c1ecf2dbaf8356d1ddd535f
Jeroen Jacobs <jacobsjeroen@gmail.com>
2010-06-01 12:11:49 +0200

    Limit amount of partners to 6

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -2,6 +2,6 @@ class Partner < ActiveRecord::Base
   upload_column :logo
 
   named_scope :active, :conditions => {:active => true}
-  named_scope :ordered, :order => 'forced_position DESC, rand()'
+  named_scope :ordered, :order => 'forced_position DESC, rand()', :limit => 6
   named_scope :by_language, lambda {|language| {:conditions => ["language = :language OR language = '' OR language IS NULL", {:language => language.to_s}]}}
 end
