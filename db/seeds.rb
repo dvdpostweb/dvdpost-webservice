eb5db624319ee6503351f3855fa6e5b5652aa711
guillaume sartenaer <gs@dvdpost.be>
2010-06-03 14:25:08 +0200

    listedProductmodel

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #   
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Major.create(:name => 'Daley', :city => cities.first)
+
+ListedProduct.delete_all("product_list_id = 1 ")
+[129,1232,91,115546,8715,3901,124400,983,123486,104216].each_with_index do |product_id, i|
+  ListedProduct.create(:product_id => product_id, :product_list_id => 1, :order => (i+1))
+end
