23d5e3d61ed7d99bc9609da487d517ef8ffffe84
Jeroen Jacobs <jacobsjeroen@gmail.com>
2010-06-04 10:18:09 +0200

    Implemented sliced design for profile page with all known data. New legacy data should be added.

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,6 +1,10 @@
 class CustomersController < ApplicationController
   before_filter :set_body_id
 
+  def show
+    @customer = current_customer
+  end
+
   private
   def set_body_id
     @body_id = 'moncompte'
