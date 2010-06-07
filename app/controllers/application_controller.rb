664ff794c6cfe40b28997c53c29af8f78be09845
Maxime Robert-Schreyers <mrs@LeFatPro.local>
2010-06-07 11:13:30 +0200

    fixed a bug in locale setting: crashed if you tried to access I18n backend without haveing a locale in your session yet

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,26 @@ class ApplicationController < ActionController::Base
   before_filter :messages_size
   before_filter :load_partners
 
+  before_filter :set_locale_from_params
+
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
+  protected
+  def set_locale_from_params
+    logger.debug "* Available locales are: #{available_locales.inspect}"
+    set_locale(extract_locale_from_params || I18n.default_locale)
+    logger.debug "* Locale set to '#{I18n.locale}'"
+  end
 
+  def available_locales
+    AVAILABLE_LOCALES
+  end
+
+  def extract_locale_from_params
+    (available_locales.include? params[:locale]) ? params[:locale] : nil
+  end
+  def default_url_options(options={})
+    logger.debug "default_url_options is passed options: #{options.inspect}\n"
+    options.keys.include?(:locale) ? options : options.merge( :locale => I18n.locale )
+  end 
 end
