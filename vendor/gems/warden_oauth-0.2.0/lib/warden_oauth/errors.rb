e4b5b13d09e470bcf7507420135fef017f94dab8
Maxime Robert-Schreyers <mrs@redstorm.be>
2010-06-23 11:50:32 +0200

    removed vendored gems, switched to submodules

--- vendor/gems/warden_oauth-0.2.0/lib/warden_oauth/errors.rb
+++
@@ -1,9 +0,0 @@
-module Warden
-  module OAuth2
-
-    class ConfigError < ArgumentError; end
-    class ServiceAlreadyRegistered < StandardError; end
-    class AccessTokenFinderMissing < StandardError; end
-
-  end
-end
