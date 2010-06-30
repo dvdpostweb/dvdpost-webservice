7df9f2bb56a579d6e79e78c6de864aa7fba176b1
Jeroen Jacobs <jacobsjeroen@gmail.com>
2010-06-30 14:47:59 +0200

    Formatting

--- db/migrate/20100430122439_landinglogin.rb
+++ db/migrate/20100430122439_landinglogin.rb
@@ -1,9 +1,9 @@
 class Landinglogin < ActiveRecord::Migration
   def self.up
-	add_column :landings, :login, :boolean, :default => 0
+    add_column :landings, :login, :boolean, :default => 0
   end
 
   def self.down
-	remove_column :landings, :login
+    remove_column :landings, :login
   end
 end
