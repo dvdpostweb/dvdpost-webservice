807fa4e73f40d56213d3838617d6494fc1276b40
guillaume <gs@dvdpost.be>
2010-07-01 16:22:30 +0200

    new button + debuging

--- app/models/contest_name.rb
+++ app/models/contest_name.rb
@@ -4,7 +4,8 @@ class ContestName < ActiveRecord::Base
   set_primary_key :contest_name_id
 
   named_scope :by_language, lambda {|language| {:conditions => {(language == :nl ? :active_nl : (language == :en ? :active_en : :active_fr)) => 1}}}
-
+  named_scope :by_date, :conditions => "validity > now()"
+  named_scope :ordered, :order => "validity asc"
   def image
     File.join(DVDPost.images_language_path[I18n.locale], banner) if  !banner.empty?
   end
