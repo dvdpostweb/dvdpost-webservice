955c818e09714879f484bf2d23da87a4380dbec9
Jeroen Jacobs <jacobsjeroen@gmail.com>
2010-07-19 15:15:19 +0200

    Implemented support for serialization on filter array fields

--- app/models/filter.rb
+++ app/models/filter.rb
@@ -1,4 +1,11 @@
 class Filter < ActiveRecord::Base
+  serialize :audio
+  serialize :subtitles
+  serialize :media
+  serialize :recommended_ids
+
+  before_save :transform_hashes_to_arrays
+
   def audience?
     audience_min? || audience_max?
   end
@@ -9,5 +16,11 @@ class Filter < ActiveRecord::Base
 
   def year?
     year_min? || year_max?
+  end
+
+  def transform_hashes_to_arrays
+    self.audio = audio.keys.collect{|key| key.to_i} if audio? && audio.respond_to?(:keys)
+    self.subtitles = subtitles.keys.collect{|key| key.to_i} if subtitles? && subtitles.respond_to?(:keys)
+    self.media = media.symbolize_keys.keys if media? && media.respond_to?(:symbolize_keys)
   end
 end
