53818935b3f67edb333680e3b42443a4c22a5a50
Jeroen Jacobs <jacobsjeroen@gmail.com>
2010-07-19 15:13:46 +0200

    Fixed filters create migration. Adds a recommended_ids and changes some fields from string to text in order to save serialized arrays

--- db/migrate/20100716094410_create_filters.rb
+++ db/migrate/20100716094410_create_filters.rb
@@ -1,25 +1,26 @@
 class CreateFilters < ActiveRecord::Migration
   def self.up
     create_table :filters do |t|
+      t.references :customer
       t.string :search_term
       t.string :view_mode
       t.integer :actor_id
-      t.integer :audience_min
-      t.integer :audience_max
       t.integer :category_id
       t.integer :country_id
       t.integer :director_id
-      t.string :media
+      t.integer :audience_min
+      t.integer :audience_max
       t.integer :rating_min
       t.integer :rating_max
       t.integer :year_min
       t.integer :year_max
       t.integer :top_id
       t.integer :theme_id
-      t.string :audio
-      t.string :subtitles
+      t.text :audio
+      t.text :subtitles
+      t.text :media
+      t.text :recommended_ids
       t.boolean :dvdpost_choice
-      t.references :customer
       t.timestamps
     end
   end
