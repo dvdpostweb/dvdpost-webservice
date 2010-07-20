40f2ab05956590a1d46ba23c37f4d2974a0aa370
Jeroen Jacobs <jacobsjeroen@gmail.com>
2010-07-20 17:06:16 +0200

    Implemented destroy action on filters

--- app/controllers/filters_controller.rb
+++ app/controllers/filters_controller.rb
@@ -1,12 +1,12 @@
 class FiltersController < ApplicationController
   def create
-    @filter = current_customer.filter || current_customer.build_filter
-    @filter.update_attributes(params[:filter])
+    filter = current_customer.filter || current_customer.build_filter
+    filter.update_attributes(params[:filter])
     redirect_to products_path(:search => params[:search])
   end
 
   def destroy
-    current_customer.filter
+    current_customer.filter.destroy
     redirect_to :back
   end
 end
