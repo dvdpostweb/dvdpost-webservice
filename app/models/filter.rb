471abb1e247215fe5ea92c6e968d478ba09a6a4d
Jeroen Jacobs <jacobsjeroen@gmail.com>
2010-07-20 13:42:45 +0200

    Filters on the left side are now opened and closed when they should

--- app/models/filter.rb
+++ app/models/filter.rb
@@ -7,15 +7,15 @@ class Filter < ActiveRecord::Base
   before_save :transform_hashes_to_arrays
 
   def audience?
-    audience_min? || audience_max?
+    (audience_min? || audience_max?) && !(audience_min == 0 && audience_max == 18)
   end
 
   def rating?
-    rating_min? || rating_max?
+    (rating_min? || rating_max?) && !(rating_min == 0 && rating_max == 5)
   end
 
   def year?
-    year_min? || year_max?
+    (year_min? || year_max?) && !(year_min == 0 && year_max >= Time.now.year)
   end
 
   def transform_hashes_to_arrays
