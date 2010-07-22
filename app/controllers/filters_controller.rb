2c73eb78784aaab1beb0d8a5216954ad8021449e
Jeroen Jacobs <jacobsjeroen@gmail.com>
2010-07-22 11:28:12 +0200

    Filter now works on all view modes

--- app/controllers/filters_controller.rb
+++ app/controllers/filters_controller.rb
@@ -2,7 +2,7 @@ class FiltersController < ApplicationController
   def create
     filter = current_customer.filter || current_customer.build_filter
     filter.update_attributes(params[:filter])
-    redirect_to products_path(:search => params[:search])
+    redirect_to products_path(:view_mode => params[:view_mode], :search => params[:search])
   end
 
   def destroy
