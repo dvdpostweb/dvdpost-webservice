0f150ef9398223f2d8d5daa154b252d530bb4c8a
Maxime Robert-Schreyers <mrs@redstorm.be>
2010-07-22 16:46:05 +0200

    Carousel pagination works

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -6,7 +6,7 @@ class QuizzesController < ApplicationController
 
   def show
     @quizz_name = QuizzName.find(params[:id])
-    @previous_list = QuizzName.previous_list(4)
+    @previous_list = QuizzName.previous_list.paginate(:per_page => 4, :page => params[:page])
   end
   
 end
