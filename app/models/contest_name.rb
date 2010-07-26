b38d5ebc0a8df126302f0f7937b7401d817dd207
Jeroen Jacobs <jacobsjeroen@gmail.com>
2010-07-26 15:35:09 +0200

    Formatting

--- app/models/contest_name.rb
+++ app/models/contest_name.rb
@@ -1,17 +1,16 @@
 class ContestName < ActiveRecord::Base
   set_table_name :contest_name
-
   set_primary_key :contest_name_id
 
   named_scope :by_language, lambda {|language| {:conditions => {(language == 'nl' ? :active_nl : (language == 'en' ? :active_en : :active_fr)) => 1}}}
-  named_scope :by_date, :conditions => "validity > now()"
-  named_scope :ordered, :order => "validity asc"
+  named_scope :by_date, :conditions => "validity > NOW()"
+  named_scope :ordered, :order => "validity ASC"
+
   def image
-    File.join(DVDPost.images_language_path[I18n.locale], banner) if !banner.empty?
+    File.join(DVDPost.images_language_path[I18n.locale], banner) unless banner.empty?
   end
 
   def image_big
-    File.join(DVDPost.images_language_path[I18n.locale], jaquette) if !jaquette.empty?
+    File.join(DVDPost.images_language_path[I18n.locale], jaquette) unless jaquette.empty?
   end
-
 end
