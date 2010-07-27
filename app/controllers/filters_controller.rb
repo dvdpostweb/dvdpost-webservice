7087f9f3e53c9bdd587c43b9d9672cd2dbfc7605
guillaume <gs@dvdpost.be>
2010-07-27 09:37:58 +0200

    separation between search and the rest

--- app/controllers/filters_controller.rb
+++ app/controllers/filters_controller.rb
@@ -2,7 +2,11 @@ class FiltersController < ApplicationController
   def create
     filter = current_customer.filter || current_customer.build_filter
     filter.update_with_defaults(params[:filter])
-    redirect_to products_path(:view_mode => params[:view_mode], :list_id => params[:list_id], :category_id => params[:category_id], :search => params[:search])
+    if params[:search] == t('products.left_column.search')
+      redirect_to products_path(:view_mode => params[:view_mode], :list_id => params[:list_id], :category_id => params[:category_id])
+    else
+      redirect_to products_path(:search => params[:search])
+    end
   end
 
   def destroy
