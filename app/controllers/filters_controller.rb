eaf8baab3d85d83e4ac1fcf22b5b6c015976b5f2
Jeroen Jacobs <jacobsjeroen@gmail.com>
2010-07-28 16:15:34 +0200

    Fixed an error where you would get an exception when doing a search from products#show

--- app/controllers/filters_controller.rb
+++ app/controllers/filters_controller.rb
@@ -1,7 +1,9 @@
 class FiltersController < ApplicationController
   def create
-    filter = current_customer.filter || current_customer.build_filter
-    filter.update_with_defaults(params[:filter])
+    if params[:filter]
+      filter = current_customer.filter || current_customer.build_filter
+      filter.update_with_defaults(params[:filter])
+    end
     if params[:search] == t('products.left_column.search')
       redirect_to products_path(:view_mode => params[:view_mode], :list_id => params[:list_id], :category_id => params[:category_id])
     else
