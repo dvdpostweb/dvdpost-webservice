a4d9b5593410e3d5747a825937c36c8bbca98c4f
guillaume <gs@dvdpost.be>
2010-07-29 17:36:31 +0200

    sponsorships

---
+++ app/models/email.rb
@@ -0,0 +1,9 @@
+class Email < ActiveRecord::Base
+  set_table_name :mail_messages
+  set_primary_key :mail_messages_id
+
+  alias_attribute :body, :messages_html
+  alias_attribute :subject, :messages_subject
+
+  named_scope :by_language, lambda {|language| {:conditions => {:language_id => DVDPost.product_languages[language]}}}
+end
