b5601c7eecc6b40ef4ded32e598e9bd1ccc68995
guillaume <gs@dvdpost.be>
2010-09-14 11:58:52 +0200

    token string

--- app/helpers/streaming_products_helper.rb
+++ app/helpers/streaming_products_helper.rb
@@ -10,7 +10,7 @@ module StreamingProductsHelper
       plugins: {
         softlayer: {
           url: '/flowplayer/flowplayer.rtmp-3.1.3.swf',
-          netConnectionUrl: '#{CDN.connect_url(token.token)}'
+          netConnectionUrl: '#{CDN.connect_url(token.token.to_s)}'
         }
       }
     });
