56baf6c6557e2154a73ed35d660fa0457c93d3a1
guillaume <gs@dvdpost.be>
2010-09-14 12:02:29 +0200

    filename

--- app/helpers/streaming_products_helper.rb
+++ app/helpers/streaming_products_helper.rb
@@ -1,5 +1,5 @@
 module StreamingProductsHelper
-  def flowplayer(source_file, token)
+  def flowplayer(source_file, token_name)
     script = <<-script
     $f("player", "/flowplayer/flowplayer-3.1.5.swf", {
       clip: {
@@ -10,7 +10,7 @@ module StreamingProductsHelper
       plugins: {
         softlayer: {
           url: '/flowplayer/flowplayer.rtmp-3.1.3.swf',
-          netConnectionUrl: '#{CDN.connect_url(token.token.to_s)}'
+          netConnectionUrl: '#{CDN.connect_url(token_name)}'
         }
       }
     });
