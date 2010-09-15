3d1dd128a854a61a10951c76492fa72b5ab8274b
guillaume <gs@dvdpost.be>
2010-09-15 17:34:33 +0200

    cleaning

--- app/models/token.rb
+++ app/models/token.rb
@@ -14,8 +14,8 @@ class Token < ActiveRecord::Base
   def self.validate(token_param, filename, ip)
     
     token = self.available.find_by_token(token_param)
-    if(token)
-      filename = filename.sub(/_low\.mp4/,'.mp4')..sub(/_high\.mp4/,'.mp4')
+    if token
+      filename = filename.sub(/_low\.mp4/,'.mp4').sub(/_high\.mp4/,'.mp4')
       filename_select = StreamingProduct.by_filename(filename).all(:include => :tokens, :conditions => ['tokens.id = ?', token.id])
       token_ips = token.token_ips
       select = token_ips.find_by_ip(ip)
