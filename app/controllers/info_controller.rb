d63d308ce0de89b473a466b9c05a9ded68fec71a
guillaume <gs@dvdpost.be>
2010-12-13 15:18:12 +0100

    get connected visible without login

--- app/controllers/info_controller.rb
+++ app/controllers/info_controller.rb
@@ -1,2 +1,16 @@
 class InfoController < ApplicationController
+
+  skip_before_filter :authenticate! 
+
+  def index
+    if params[:page_name] == 'get_connected'
+      unless current_customer
+        @hide_menu = true
+      end
+    end
+  end
+  protected
+  def is_customer?
+    
+  end
 end
