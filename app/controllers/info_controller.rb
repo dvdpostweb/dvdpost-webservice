6cd695320aef53a7d4bbfd244f04a12ad463b725
guillaume <gs@dvdpost.be>
2010-12-22 16:26:07 +0100

    get_connect_order not log

--- app/controllers/info_controller.rb
+++ app/controllers/info_controller.rb
@@ -3,7 +3,7 @@ class InfoController < ApplicationController
   skip_before_filter :authenticate! 
 
   def index
-    if params[:page_name] == 'get_connected'
+    if params[:page_name] == 'get_connected' || params[:page_name] == 'get_connected_order'
       unless current_customer
         @hide_menu = true
       end
