43ffb341dfa25479e891aecf0079beeab4e5dddb
guillaume <gs@dvdpost.be>
2011-07-08 16:03:14 +0200

    change schedule hour

--- config/schedule.rb
+++ config/schedule.rb
@@ -14,14 +14,25 @@
 # end
 #
  set :output, './log/cron.log'
-
+ every 1.day, :at => '10:00 am' do
+   runner "HighlightCustomer.add_point_by_day"
+ end
+ every 1.day, :at => '10:30 am' do
+   runner "HighlightCustomer.add_point"
+ end
  every 1.day, :at => '10:55 am' do
    runner "HighlightReview.run_reviews"
+ end
+ every 1.day, :at => '11:00 am' do
    runner "HighlightProduct.run_best_rating"
+ end
+ every 1.day, :at => '11:05 am' do
    runner "HighlightProduct.run_controverse_rating"
-   runner "HighlightCustomer.add_point_by_day"
-   runner "HighlightCustomer.add_point"
+ end
+ every 1.day, :at => '11:10 am' do
    runner "HighlightCustomer.run_best_customer_all"
+ end
+ every 1.day, :at => '11:15 am' do
    runner "HighlightCustomer.run_best_customer_month"
  end
 
