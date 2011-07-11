5bcf4d53bda775dcde19750f379f436659719f16
guillaume <gs@dvdpost.be>
2011-07-11 11:03:25 +0200

    change rules and add log

--- config/schedule.rb
+++ config/schedule.rb
@@ -23,16 +23,16 @@
  every 1.day, :at => '11:55 am' do
    runner "HighlightReview.run_reviews"
  end
- every 1.day, :at => '12:00 am' do
+ every 1.day, :at => '12:05 pm' do
    runner "HighlightProduct.run_best_rating"
  end
- every 1.day, :at => '12:05 am' do
+ every 1.day, :at => '12:05 pm' do
    runner "HighlightProduct.run_controverse_rating"
  end
- every 1.day, :at => '12:10 am' do
+ every 1.day, :at => '12:10 pm' do
    runner "HighlightCustomer.run_best_customer_all"
  end
- every 1.day, :at => '12:15 am' do
+ every 1.day, :at => '12:15 pm' do
    runner "HighlightCustomer.run_best_customer_month"
  end
 
