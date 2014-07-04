b59d634d7fca03acddf77c189978c1e25ed0dbd3
guillaume sartenaer <tiguss@gmail.com>
2014-07-05 01:37:29 +0200

    zencoder

--- app/models/streaming_product.rb
+++ app/models/streaming_product.rb
@@ -5,22 +5,54 @@ class StreamingProduct < ActiveRecord::Base
   def self.zen_coder
     
     myFile = File.open("log/myFile.txt", "w")
-    5.times do |i|
+    sql = 'select x.*, pl.short_alpha short_lang, ifnull(pu.short_alpha,'non') short_sub
+from
+(select distinct t.imdb_id, language_id, subtitle_id, filename, quality
+from plush_staging.tokens t 
+join plush_staging.products p on t.imdb_id = p.imdb_id and p.products_type = "DVD_NORM"
+join streaming_products sp on sp.imdb_id = p.imdb_id and studio_id !=750 and (`expire_backcatalogue_at` > now() or expire_backcatalogue_at is null) and available = 1 and (subtitle_id in (1,2) or language_id in (1,2))
+where t.created_at > date_add(now(), interval -14 month) and compensed=0
+union all
+select distinct t.imdb_id, language_id, subtitle_id, filename, quality
+
+from plush_staging.tokens t 
+join plush_staging.products p on t.imdb_id = p.imdb_id and p.products_type = "DVD_NORM"
+join streaming_products sp on sp.imdb_id = p.imdb_id and studio_id !=750 and (`expire_backcatalogue_at` > now() or expire_backcatalogue_at is null) and available = 1 and (subtitle_id in (1,2) or language_id in (1,2))
+
+where t.created_at > date_add(now(), interval -14 month) and compensed=0
+) x
+left join products_undertitles pu on subtitle_id = undertitles_id and pu.language_id=1
+join products_languages pl on x.language_id = pl.languages_id and languagenav_id=1
+group by x.imdb_id, subtitle_id,language_id
+order by x.imdb_id limit 10'
+    results = ActiveRecord::Base.connection.execute(sql)
+    results.each_hash do |h| 
+      	case h['quality']
+      	when '1080p'
+      		bitrate = 3000
+      		width =1920
+      	when '720p'
+      		bitrate = 2500
+      		width =1024
+      	else
+      		bitrate = 2000
+      		width =720
+      	end
       var = <<-EOF
-    {#{i}
+    {
       "api_key": "6541e219a225b48e901393d73d906091",
-      "input": "sftp://PS3user:ps3%40DVDPOST2014@94.139.62.130:22/home/PS3user/dvdpost/12Y_SLAVE_2013@2024544_Dpc_Aeng_Sfre_3000k.f4v",
+      "input": "sftp://PS3user:ps3%40DVDPOST2014@94.139.62.130:22/home/PS3user/dvdpost/#{h['filename']}@#{h['imdb_id']}_Dpc_A#{h['short_lang']}_S#{h['short_sub']}_3000k.f4v",
       "region": "europe",
       "outputs":
       {
         "audio_bitrate": 64,
         "audio_sample_rate": 48000,
-        "url": "ftp://hesssvodupload:HESssvod123@homessvod.upload.akamai.com/308707/2024544_Aeng_Sfre.ism",
+        "url": "ftp://hesssvodupload:HESssvod123@homessvod.upload.akamai.com/308707/#{h['imdb_id']}_A#{h['short_lang']}_S#{h['sub']}.ism",
         "max_frame_rate": 25,
         "segment_seconds": 2,
         "type": "segmented",
-        "video_bitrate": 3000,
-        "width": 1920,
+        "video_bitrate": #{bitrate},
+        "width": #{width},
         "format": "ism",
         "drm":    
         {
@@ -28,7 +60,7 @@ class StreamingProduct < ActiveRecord::Base
           "provider": "buydrm",
           "server_key": "89137186-356A-4543-B83F-943699CBD44E",
           "user_key": "d88e502b-fd8f-4da5-e79b-ea73d132b89f",
-          "media_id": "2024544_Aeng_Sfre"
+          "media_id": "h['imdb_id']_Ah['short_lang']_Sh['short_sub']"
         }
       }
     }
