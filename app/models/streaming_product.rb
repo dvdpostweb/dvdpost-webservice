1f9263f88d2cafcf0046b4133f4ba7a672171338
it dvdpost <it@dvdpost.be>
2015-09-01 20:58:12 +0200

    Igor: Change in zen_coder_s
    Change in zen_coder_s to read files from akamai HLS location. sql query changed also

--- app/models/streaming_product.rb
+++ app/models/streaming_product.rb
@@ -152,27 +152,23 @@ def self.zen_coder_s
     puts body
     if  status_input == 'finished' || status_input == 'failed'
       puts 'launch new process'
-      sql = 'select x.*, pl.short_alpha short_lang, ifnull(pu.short_alpha,"non") short_sub,products_year, x.season_id , x.episode_id from 
-  (select distinct sp.created_at, sp.imdb_id, language_id, subtitle_id, filename, quality, products_year, p.season_id, p.episode_id
-            from dvdpost_be_prod.tokens t 
-            right join dvdpost_be_prod.products p on t.imdb_id = p.imdb_id
-            right join streaming_products sp on sp.imdb_id = p.imdb_id 
-            where ((t.created_at > date_add(now(), interval -14 month ) and compensed=0) or sp.created_at > "2014-01-01")  and p.products_type = "DVD_NORM" and  (studio_id !=750 || (studio_id = 750 && sp.videoland = 1)) and (`expire_backcatalogue_at` > now() or expire_backcatalogue_at is null) and available = 1 and status = "online_test_ok" and (subtitle_id in (1,2) or (language_id in (1,2) and (subtitle_id is null or subtitle_id in (1,2))))
-            union all
-            select distinct sp.created_at, sp.imdb_id, language_id, subtitle_id, filename, quality, products_year, p.season_id, p.episode_id
-
-            from plush_production.tokens t 
-            right join plush_production.products p on t.imdb_id = p.imdb_id 
-            right join streaming_products sp on sp.imdb_id = p.imdb_id and sp.season_id = p.season_id and p.episode_id = sp.episode_id 
-
-            where ((t.created_at > date_add(now(), interval -14 month) and compensed=0) or sp.created_at > "2014-01-01")  and p.products_type = "DVD_NORM" and studio_id !=750 and (`expire_backcatalogue_at` > now() or expire_backcatalogue_at is null) and available = 1 and status = "online_test_ok" and (subtitle_id in (1,2) or (language_id in (1,2) and (subtitle_id is null or subtitle_id in (1,2))))
-            ) x
-            left join products_undertitles pu on subtitle_id = undertitles_id and pu.language_id=1
-            join products_languages pl on x.language_id = pl.languages_id and languagenav_id=1
-            left join akamai_jobs a on a.imdb_id = x.imdb_id and x.season_id = a.season_id and x.episode_id = a.episode_id and a.language_id = x.language_id and ifnull(a.subtitle_id,0) = ifnull(x.subtitle_id,0) and deleted = 0
-            where  a.id is null
-            group by x.imdb_id, x.season_id, x.episode_id, x.subtitle_id,x.language_id
-            order by products_year desc, x.imdb_id desc limit 1;'
+      sql = 'select x.*, pl.short_alpha short_lang,
+ ifnull(pu.short_alpha,"non") short_sub,
+ products_year, x.season_id , x.episode_id from
+(
+select distinct sp.akamai_folder, sp.created_at, sp.imdb_id, language_id, subtitle_id, filename, quality, products_year, p.season_id, p.episode_id
+            from streaming_products sp join products p on sp.imdb_id = p.imdb_id
+            where sp.created_at > date_add(now(), interval -10 day)
+            and p.products_type = 'DVD_NORM'
+            and  studio_id !=750
+            and (`expire_backcatalogue_at` > now() or expire_backcatalogue_at is null)
+            and available = 1
+            and status = 'online_test_ok'
+) x
+join products_languages pl on x.language_id = pl.languages_id and languagenav_id=1
+left join products_undertitles pu on x.subtitle_id = pu.undertitles_id and pu.language_id=1
+group by x.imdb_id, x.season_id, x.episode_id, x.subtitle_id,x.language_id
+order by products_year desc, x.imdb_id desc limit 1;'
       results = ActiveRecord::Base.connection.execute(sql)
       results.each_hash do |h| 
         case h['quality']
@@ -201,7 +197,7 @@ def self.zen_coder_s
         end
         puts season_name
         puts h['season_id']
-        input = "sftp://PS3user:nomoreconnection@94.139.62.130/home/PS3user/dvdpost/#{h['filename']}#{imdb_id}_Dpc_A#{h['short_lang']}_S#{h['short_sub']}_3000k.f4v"
+         input = "ftp://vlupload:videoland123@ftp://homehlsvod.upload.akamai.com/308709/#{h['akamai_folder']}/#{h['filename']}_3000000.f4v"
         result = Zencoder::Job.create({:api_key => "6541e219a225b48e901393d73d906091", :region => "europe", :input => input, :outputs => { :audio_bitrate => 64, :audio_sample_rate => 48000, :url => "ftp://hesssvodupload:HESssvod123@homessvod.upload.akamai.com/308707/#{season_name}#{h['imdb_id']}_A#{h['short_lang']}_S#{h['short_sub']}.ism", :max_frame_rate => 25, :segment_seconds => 2, :type => "segmented", :video_bitrate => bitrate, :width => width, :format => "ism", :drm => { :method => "playready", :provider => "buydrm", :server_key => "89137186-356A-4543-B83F-943699CBD44E", :user_key => "d88e502b-fd8f-4da5-e79b-ea73d132b89f", :media_id => "#{season_name}#{h['imdb_id']}_A#{h['short_lang']}_S#{h['short_sub']}" }}}, {:skip_ssl_verify => true})
         job = AkamaiJob.create(:imdb_id => h['imdb_id'], :language_id => h['language_id'], :subtitle_id => h['subtitle_id'], :zen_id => result.body['id'], :season_id => h['season_id'], :episode_id => h['episode_id'])
         puts result.body
