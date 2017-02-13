f279c07bb1705506391e7e9ccd4b0863d5627824
popac <web.guru.serbia@gmail.com>
2017-02-13 10:28:29 +0100

    Set production deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -19,7 +19,7 @@ set :rails_env, "production"
 #	Servers
 #############################################################
 
-set :user, "dvdpost-webservice"
+set :user, "dvdpost"
 set :domain,  "217.112.190.50" #tokyo
 #set :domain2,  "192.168.100.221" #stockholm
 set :port, 23051
